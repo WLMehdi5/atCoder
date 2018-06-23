0324ede1230d701c0f5af5f49b07518a478f60a9
WLMehdi5 <WL.Mehdi5@gmail.com>
2018-06-23 22:43:16 +0900

    Revert "6/23参加"
    This reverts commit eadb8841d2995054363eb4ee03b31c51aef9956d.

--- ABC101/Digit Sums.py
+++
@@ -1,8 +0,0 @@
-S = input()
-x=0
-for i in range(len(S)):
-    x +=int(S[i])
-if int(S) % x ==0:
-    print('Yes')
-else:
-    print('No')
--- ABC101/EatingSymbols.py
+++
@@ -1,8 +0,0 @@
-S = input()
-x=0
-for i in range(len(S)):
-    if S[i] == '+':
-        x +=1
-    else:
-        x -=1
-print(x)
